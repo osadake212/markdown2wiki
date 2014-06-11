874368b35a321eac7d065af7e532c03db48c27d1
Osada Takuya <osadake.212.dev@gmail.com>
2014-06-11 10:02:09 +0900

    add error handling at convert and io processing

--- libs/converter.rb
+++ libs/converter.rb
@@ -23,18 +23,27 @@ class Converter
 				return
 			end
 
-			# ファイル読み込み + 変換
-			new_text = ""
-			File.open(markdown, encoding: "utf-8").each do |line|
-				new_text += line.convert_markdown_to_wiki(markdown_config)
-			end
+			begin
+				# ファイル読み込み + 変換
+				new_text = ""
+				File.open(markdown, encoding: "utf-8").each do |line|
+					new_text += line.convert_markdown_to_wiki(markdown_config)
+				end
 
-			# ファイル書き込み
-			File.open(wiki, 'w:utf-8'){ |f|
-				f.write(new_text)
-			}
-
+				# ファイル書き込み
+				File.open(wiki, 'w:utf-8'){ |f|
+					f.write(new_text)
+				}
 			#puts new_text
+			rescue
+				if $!
+					puts 'エラーが発生しました。'
+					puts 'エラー内容：'
+					puts $!.message				# エラーメッセージ
+					puts $!.backtrace			# スタックトレース
+					IO.write(ERROR_LOG, [$!.message, $!.backtrace].join("\n"))
+				end
+			end
 		end
 
 	end
